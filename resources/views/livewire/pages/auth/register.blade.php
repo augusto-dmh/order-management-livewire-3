f64250d00b544fbf634962cdb39c1a98848c4f74
= <augustodemeloh@protonmail.com>
2025-03-19 02:01:53 -0300

    fix: password fields not synchronized between livewire components
    Instead of taking Povillas approach, i ended up installing Breeze starter kit with livewire components instead of the default blade ones, and then i've faced the problem that there was a problem related to component communication: 'RegisterPasswords' and 'register' had both the password property - the commit doesn't show this, but all this started with getting an error of password property not defined, defining it but then facing a new and silent problem involving synchronization -, but they didn't shared any reference: password from 'RegisterPasswords' was making data binding happen (the password strength progress bar working etc.), but the password that was getting validated in 'register' wasn't any related to it. After exploring the possibilities, i concluded letting all the business logic related to the form in a single component and just encapsulate the password structure in a blade component rather than another livewire component would be the most suitable approach: the actions on the livewire component would be propagated to the blade component on re-rendering it. Considering the size of the component, nesting would bring more overhead than advantages: implementing a workaround to solve the defficiency of Livewire not out-of-the-box having how to "establish communication" between components through prop changes wouldn't be worthwhile. I've read the documentation: each component it's an island, so any change on the props of one component wouldn't affect at all any nesting one.

--- resources/views/livewire/pages/auth/register.blade.php
+++ resources/views/livewire/pages/auth/register.blade.php
@@ -5,14 +5,45 @@ use App\Providers\RouteServiceProvider;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 use Illuminate\Validation\Rules;
 use Livewire\Attributes\Layout;
 use Livewire\Volt\Component;
+use ZxcvbnPhp\Zxcvbn;
 
 new #[Layout('layouts.guest')] class extends Component
 {
     public string $name = '';
     public string $email = '';
+    public string $password = '';
+    public string $password_confirmation = '';
+    public int $strengthScore = 0;
+    public array $strengthLevels = [
+        1 => 'Weak',
+        2 => 'Fair',
+        3 => 'Good',
+        4 => 'Strong',
+    ];
+
+    public function generatePassword(): void
+    {
+        $password = Str::password(12);
+
+        $this->password = $password;
+        $this->password_confirmation = $password;
+
+        $this->updateStrengthScore();
+    }
+
+    public function updatedPassword($value)
+    {
+        $this->updateStrengthScore();
+    }
+
+    protected function updateStrengthScore(): void
+    {
+        $this->strengthScore = (new Zxcvbn())->passwordStrength($this->password)['score'];
+    }
 
     /**
      * Handle an incoming registration request.
@@ -40,18 +71,18 @@ new #[Layout('layouts.guest')] class extends Component
         <!-- Name -->
         <div>
             <x-input-label for="name" :value="__('Name')" />
-            <x-text-input wire:model="name" id="name" class="block w-full mt-1" type="text" name="name" required autofocus autocomplete="name" />
+            <x-text-input wire:model="name" id="name" class="block w-full mt-1" type="text" name="name" autofocus autocomplete="name" />
             <x-input-error :messages="$errors->get('name')" class="mt-2" />
         </div>
 
         <!-- Email Address -->
         <div class="mt-4">
             <x-input-label for="email" :value="__('Email')" />
-            <x-text-input wire:model="email" id="email" class="block w-full mt-1" type="email" name="email" required autocomplete="username" />
+            <x-text-input wire:model="email" id="email" class="block w-full mt-1" type="email" name="email" autocomplete="username" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
-        @livewire('register-passwords')
+        <x-register-passwords :$strengthLevels :$strengthScore />
 
         <div class="flex items-center justify-end mt-4">
             <a class="text-sm text-gray-600 underline rounded-md hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}" wire:navigate>
