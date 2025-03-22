6a1d97f54a32110961624d76ad6d9961ab7724b2
= <augustodemeloh@protonmail.com>
2025-03-22 12:19:53 -0300

    feat: Create New Category Modal (instrutor's version)

--- app/Livewire/CategoriesList.php
+++ app/Livewire/CategoriesList.php
@@ -4,11 +4,46 @@ namespace App\Livewire;
 
 use Livewire\Component;
 use App\Models\Category;
+use Illuminate\Support\Str;
 use Livewire\WithPagination;
 
 class CategoriesList extends Component
 {
     use WithPagination;
+
+    public ?Category $category = null;
+
+    public string $name = '';
+    public string $slug = '';
+
+    public bool $showModal = false;
+
+    public function openModal(): void
+    {
+        $this->showModal = true;
+    }
+
+    public function save()
+    {
+        $this->validate();
+
+        Category::create($this->only('name', 'slug'));
+
+        $this->reset('name', 'slug', 'showModal');
+    }
+
+    public function updatedName(): void
+    {
+        $this->slug = Str::slug($this->name);
+    }
+
+    protected function rules(): array
+    {
+        return [
+            'name' => ['required', 'string', 'min:3'],
+            'slug' => ['nullable', 'string'],
+        ];
+    }
 
     public function render()
     {
