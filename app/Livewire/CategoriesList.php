4b9ebc4d10b1d178eee7ece16c9b9db70bb7599a
= <augustodemeloh@protonmail.com>
2025-03-26 01:03:32 -0300

    feat: Category Delete with SweetAlert Confirm (my version)

--- app/Livewire/CategoriesList.php
+++ app/Livewire/CategoriesList.php
@@ -25,6 +25,8 @@ class CategoriesList extends Component
 
     public int $editedCategoryId = 0;
 
+    public ?int $categoryToBeDeletedId = null;
+
     public int $currentPage = 1;
 
     public int $perPage = 10;
@@ -32,6 +34,22 @@ class CategoriesList extends Component
     public function openModal(): void
     {
         $this->showModal = true;
+    }
+
+    public function deleteCategory(int $categoryId): void
+    {
+        $categoryToBeDeleted = Category::find($categoryId);
+
+        if ($categoryToBeDeleted) {
+            $categoryToBeDeleted->delete();
+        }
+
+        $this->cancelCategoryDeletion();
+    }
+
+    public function cancelCategoryDeletion(): void
+    {
+        $this->reset('categoryToBeDeletedId');
     }
 
     public function toggleIsActive(int $categoryId): void
