a5696e109c9c400f33d10a473a60def05cfde317
= <augustodemeloh@protonmail.com>
2025-04-02 11:27:54 -0300

    feat: Products Table Sorting (mix with instructor's version)

--- app/Livewire/ProductsList.php
+++ app/Livewire/ProductsList.php
@@ -25,31 +25,25 @@ class ProductsList extends Component
         ],
     ];
 
-    public $columnSorting = ['columnName' => null, 'sortingOrder' => null];
+    public $columnSorting = ['columnName' => 'name', 'sortingOrder' => 'asc'];
 
     public $categoriesPerProductToShow = 2;
 
     public ?Collection $categories = null;
     public ?Collection $countries = null;
 
-    public function setColumnSorting($columnName)
+    public function toggleColumnSorting(string $columnName): void
     {
         if ($columnName == $this->columnSorting['columnName']) {
-            if (is_null($this->columnSorting['sortingOrder'])) {
-                $this->columnSorting['sortingOrder'] = 'asc';
-                return;
-            }
             if ($this->columnSorting['sortingOrder'] == 'asc') {
                 $this->columnSorting['sortingOrder'] = 'desc';
-                return;
+            } else {
+                $this->reset('columnSorting');
             }
-            $this->columnSorting['sortingOrder'] = null;
-            $this->columnSorting['columnName'] = null;
-            return;
+        } else {
+            $this->columnSorting['sortingOrder'] = 'asc';
+            $this->columnSorting['columnName'] = $columnName;
         }
-
-        $this->columnSorting['sortingOrder'] = 'asc';
-        $this->columnSorting['columnName'] = $columnName;
     }
 
     public function updatedFilters()
@@ -90,9 +84,7 @@ class ProductsList extends Component
             ->when($this->searchColumns['price']['to'], function (Builder $q) {
                 $q->where('price', '<=', $this->searchColumns['price']['to'] * 100);
             })
-            ->when(!is_null($this->columnSorting['columnName']), function (Builder $q) {
-                $q->orderBy($this->columnSorting['columnName'], $this->columnSorting['sortingOrder']);
-            })
+            ->orderBy($this->columnSorting['columnName'], $this->columnSorting['sortingOrder'])
             ->paginate(10);
 
         return view('livewire.products-list', [
